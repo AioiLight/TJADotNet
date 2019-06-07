aefcf083c29a5ed9145a83ba79b5b834af46bdcd
AioiLight <info@aioilight.space>
2019-06-07 20:15:58 +0900

    譜面分岐の列挙型の追加。

---
+++ TJADotNet/Format/Branches.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TJADotNet.Format
+{
+    /// <summary>
+    /// 譜面分岐の列挙型。
+    /// </summary>
+    public enum Branches
+    {
+        /// <summary>
+        /// 普通譜面
+        /// </summary>
+        Normal,
+        /// <summary>
+        /// 玄人譜面
+        /// </summary>
+        Expert,
+        /// <summary>
+        /// 達人譜面
+        /// </summary>
+        Master
+    }
+}
